7b811d598aba88c13b095d5a691540343d221d7f
Omer Degirmenci <rumidegirmenci0@gmail.com>
2022-07-30 19:27:42 +0200

    items shadow ipv border, add disabled property, change add todo button colour

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,37 +1,41 @@
-import React from 'react'
-import { Button, Form } from 'react-bootstrap'
-import { useTodoList } from './../contexts/AllTodosContext'
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { useTodoList } from "./../contexts/AllTodosContext";
 
 const Input = () => {
-  const { description, setDescription, onSubmitTodo } = useTodoList()
+  const { description, setDescription, onSubmitTodo } = useTodoList();
   const handleChangeInput = (e) => {
-    e.preventDefault()
-    const { value, maxLength } = e.target
-    const limitedValue = value.slice(0, maxLength)
+    e.preventDefault();
+    const { value, maxLength } = e.target;
+    const limitedValue = value.slice(0, maxLength);
 
-    setDescription(limitedValue)
-  }
+    setDescription(limitedValue);
+  };
 
   return (
-    <Form onSubmit={onSubmitTodo} className='d-flex mt-5 mb-4'>
+    <Form onSubmit={onSubmitTodo} className="d-flex mt-5 mb-4">
       <Form.Control
-        maxLength='30'
+        maxLength="30"
         value={description}
-        type='text'
-        placeholder='Enter a new todo'
+        type="text"
+        placeholder="Enter a new todo"
         onChange={(e) => handleChangeInput(e)}
       />
       <Button
+        className="ml-3"
         style={{
-          fontSize: '16px',
+          fontSize: "16px",
+          backgroundColor: "#3c2b42",
+          marginLeft: "5px",
         }}
-        variant='outline-light'
-        type='submit'
+        variant="primary"
+        type="submit"
+        disabled={!description}
       >
-        <i className='fa fa-plus'></i>
+        <i className="fa fa-plus"></i>
       </Button>
     </Form>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
