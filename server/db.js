9cb6d3716359387d5c2a175f788d22b0d93c1236
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-01-10 22:46:29 +0100

    edit db settings

--- server/db.js
+++ server/db.js
@@ -6,11 +6,20 @@ dotenv.config()
 const { Pool } = pg
 
 const pool = new Pool({
-  user: 'postgres',
+  user: process.env.PG_USER,
   password: process.env.DB_PASSWORD,
-  host: 'localhost',
+  host: process.env.PG_HOST,
   port: 5432,
-  database: 'perntodo',
+  database: process.env.PG_DATABASE,
+  ssl: true,
 })
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: process.env.DB_PASSWORD,
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'perntodo',
+// })
 
 export default pool
